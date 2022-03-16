fdb17a76cf29ae6a635eb5313020e60ca2a60f38
s5163577 <100301188+s5163577@users.noreply.github.com>
2022-03-16 13:35:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+    float a = 0,b = 0,c = 0;
+    float area_Triangolo, area_Quadrato, area_Rettangolo, area_Trapezio;//Qui vanno definite le variabili
 
-cin >> a >> b >> c;
+    cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    area_Triangolo = (a*b)/2;
+    area_Quadrato = a*a;
+    area_Rettangolo = a*b;
+    area_Trapezio = ((a+b)*c)/2;//Qui va scritto il codice per eseguire i calcoli richiesti
 
-
+    cout << area_Triangolo << endl;
+    cout << area_Quadrato << endl;
+    cout << area_Rettangolo << endl;
+    cout << area_Trapezio << endl;
+    return 0;
 }
